8a101b1ebec02bbb17bd5fa97c7eb57043d74616
Anthony-cecconi <anthony.cecconi.91@gmail.com>
2025-04-29 10:07:21 +0200

    create le pagine e i componenti base

---
+++ src/app/pages/are/are.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-are',
+  standalone: true,
+  imports: [],
+  templateUrl: './are.component.html',
+  styleUrl: './are.component.scss'
+})
+export class AreComponent {
+
+}
---
+++ src/app/pages/sos/sos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sos',
+  standalone: true,
+  imports: [],
+  templateUrl: './sos.component.html',
+  styleUrl: './sos.component.scss'
+})
+export class SosComponent {
+
+}
