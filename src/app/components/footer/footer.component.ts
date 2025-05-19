9253f14a0b222092b3204f19f0a46b39559231fe
Anthony-cecconi <anthony.cecconi.91@gmail.com>
2025-05-19 15:14:41 +0200

    home completa

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +8,4 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+ slides: any[] = [
+    { imageUrl: 'https://picsum.photos/200', altText: 'Descrizione immagine 1', title: 'test 1' },
+    { imageUrl: 'https://picsum.photos/200', altText: 'Descrizione immagine 2', title: 'test 2' },
+    { imageUrl: 'https://picsum.photos/200', altText: 'Descrizione immagine 3', title: 'test 3' },
+  ];
 
+  currentIndex = 0;
+  private intervalId: any;
+
+  constructor() { }
+
+  goToNext(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+  }
+
+ goToPrevious(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
+
+  stopAutoplay(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
