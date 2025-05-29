78eccdee106cb4a475b3d311e09c0f8485bf1f82
Anthony-cecconi <anthony.cecconi.91@gmail.com>
2025-05-29 17:46:51 +0200

    carosello staf

--- src/app/pages/are/are.component.ts
+++ src/app/pages/are/are.component.ts
@@ -10,5 +10,46 @@ import { FooterComponent } from '../../components/footer/footer.component';
   styleUrl: './are.component.scss'
 })
 export class AreComponent {
+  staf : Staf[] =[
+    {
+      name : "Davide Galletti",
+      content : "testo di prova per Davide"
+    },
+      {
+      name : "Simone Galletti",
+      content : "testo di prova per Simone"
+    },
+        {
+      name : "Alfredo Galletti",
+      content : "testo di prova per Alfredo"
+    }
+  ]
+  select : number = 0;
+  person : Staf = this.staf[0];
 
+  next(){
+    if(this.select < this.staf.length -1){
+      this.select++
+    }else{
+      this.select = 0;
+    }
+
+    this.person = this.staf[this.select];
+  }
+
+  back(){
+    if(this.select > 0){
+      this.select--
+    }else{
+      this.select = this.staf.length -1;
+    }
+
+    this.person = this.staf[this.select];
+  }
+
+}
+
+export interface Staf{
+  name : string;
+  content : string;
 }
